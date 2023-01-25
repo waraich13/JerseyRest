de118215669faa61afeb2e360ba7fac80886b9ec
Davinderjit Singh <davinder000720@gmail.com>
2023-01-24 21:33:20 -0500

    added https response

--- src/main/java/com/dav/Resource/PeopleResource.java
+++ src/main/java/com/dav/Resource/PeopleResource.java
@@ -2,12 +2,15 @@ package com.dav.Resource;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.dav.JerseyRest.People;
 import com.dav.Repository.PeopleRepository;
@@ -16,12 +19,11 @@ import com.dav.Repository.PeopleRepository;
 public class PeopleResource {
 
 	PeopleRepository peoplerepo = null;
-	
-	
+
 	public PeopleResource() throws Exception {
 		this.peoplerepo = new PeopleRepository();
-	}
-	
+	}	
+
 	@GET
 	@Path("/all")
 	@Produces(MediaType.APPLICATION_XML)
@@ -31,26 +33,34 @@ public class PeopleResource {
 
 	}
 	
-	
+	@Context
+	private HttpServletResponse response;
 	@GET
 	@Path("/all")
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<People> GetListOfPeopleAsJson() throws Exception {
-
+		
+		if(peoplerepo == null) {
+			response.setStatus(Response.Status.CREATED.getStatusCode());
+		}
+		
 		return peoplerepo.getPeople();
 
 	}
-	
 
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/addpeople")
-	public void CreatePeople( String person) throws Exception {
-		
-		//1 ) if p is null return BadRequest..value is null
-		// 2) if person added successfully, return something(person variable with a message; a simple message(person added))
-		
-		peoplerepo.addPeople(person);
+	public Response CreatePeople(String person) throws Exception {
 
+		// 1 ) if p is null return BadRequest..value is null
+		// 2) if person added successfully, return something(person variable with a
+		// message; a simple message(person added))
+		if (person == null || person.trim().length() ==0) {
+			return Response.status(404).entity("person is null").build();
+		} else {
+			peoplerepo.addPeople(person);
+			return Response.status(200).entity("person added!").build();
+		}
 	}
 }
